fde21a51fcc08ea2dedb97ac7e4d3a3c97061bbb
Barry Richards <barry@br-web.me>
2023-10-19 17:31:29 +0100

    Linked up scripts for editor also

--- functions/scripts.php
+++ functions/scripts.php
@@ -1,17 +1,20 @@
 <?php
 
-add_theme_support('editor-styles');
-add_editor_style( get_stylesheet_directory_uri() . '/assets/dist/editor-style.css');
-
 function sapling_enqueue_scripts() {
-	wp_register_style( 'main', get_stylesheet_directory_uri() . '/assets/dist/style.css', array(), 
-	filemtime( get_stylesheet_directory() . '/assets/dist/style.css'));
+	// CSS
+	wp_register_style( 'sapling_css_main', get_stylesheet_directory_uri() . '/assets/dist/css/style.css', array(), 
+	filemtime( get_stylesheet_directory() . '/assets/dist/css/style.css'));
+	wp_enqueue_style( 'sapling_css_main' );
+	// JS
 	wp_deregister_script( 'wp-embed' );
-	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/dist/js/main.min.js', []);
-	wp_enqueue_style( 'main' );
+	wp_enqueue_script( 'sapling_js_main', get_template_directory_uri() . '/assets/dist/js/main.min.js', []);
 }
-
 add_action( 'wp_enqueue_scripts', 'sapling_enqueue_scripts' );
+
+function sapling_enqueue_editor_scripts(){
+	wp_enqueue_script( 'blockeditor_scripts', get_template_directory_uri() . '/assets/dist/js/block-editor.min.js', array(), '1.0.0', true );
+}
+add_action('enqueue_block_editor_assets', 'sapling_enqueue_editor_scripts');
 
 
 /**
--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,26 @@
 import { terser } from "rollup-plugin-terser";
 
-export default {
-  input: 'assets/src/js/index.js',
-  output: {
-    file: 'assets/dist/js/main.min.js',
-    format: 'iife', // immediately-invoked function expression — suitable for <script> tags
-    sourcemap: true
+// Define multiple input and output options
+const configs = [
+  {
+    input: 'assets/src/js/index.js',
+    output: {
+      file: 'assets/dist/js/main.min.js',
+      format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+      sourcemap: true
+    },
+    plugins: [terser()] // Minify the output JS
   },
-  plugins: [terser()] // Minify the output JS
-};
+  {
+    input: 'assets/src/js/block-editor.js',
+    output: {
+      file: 'assets/dist/js/block-editor.min.js',
+      format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+      sourcemap: true
+    },
+    plugins: [terser()] // Minify the output JS
+  }
+];
+
+// Export as an array of configurations
+export default configs;
