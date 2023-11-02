72d4169910a2aff465fed9d917d4d8f8b87f4750
Barry Richards <barry@br-web.me>
2023-11-02 16:19:38 +0000

    Styling updates

--- functions/scripts.php
+++ functions/scripts.php
@@ -1,21 +1,27 @@
 <?php
 
+/**
+ * @desc Enqueue scripts and styles to the front-end
+ */
 function sapling_enqueue_scripts() {
 	// CSS
-	wp_register_style( 'sapling_css_main', get_stylesheet_directory_uri() . '/assets/dist/css/style.css', array(), 
+	wp_enqueue_style( 'sapling_css_main', get_stylesheet_directory_uri() . '/assets/dist/css/style.css', array(),
 	filemtime( get_stylesheet_directory() . '/assets/dist/css/style.css'));
-	wp_enqueue_style( 'sapling_css_main' );
 	// JS
 	wp_deregister_script( 'wp-embed' );
 	wp_enqueue_script( 'sapling_js_main', get_template_directory_uri() . '/assets/dist/js/main.min.js', []);
 }
 add_action( 'wp_enqueue_scripts', 'sapling_enqueue_scripts' );
 
+/**
+ * @desc Enqueue scripts and styles to the back-end
+ */
 function sapling_enqueue_editor_scripts(){
 	wp_enqueue_script( 'blockeditor_scripts', get_template_directory_uri() . '/assets/dist/js/block-editor.min.js', array(), '1.0.0', true );
+	wp_enqueue_style( 'sapling_css_main', get_stylesheet_directory_uri() . '/assets/dist/css/editor-theme-styles.css', array(), 
+	filemtime( get_stylesheet_directory() . '/assets/dist/css/editor-theme-styles.css'));
 }
 add_action('enqueue_block_editor_assets', 'sapling_enqueue_editor_scripts');
-
 
 /**
  * @desc add defer to scripts.min.js
